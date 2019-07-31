1050b6d1bb7cae78daa4cbaec246a33f54e87c55
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2019-07-31 23:45:24 +0530

    add README.md file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# TEST PROJECT
+# Basic Calculator
+
+1. This is a basic calculator to addition, subtraction, multiplication, and division in `main.c`. \
+2. This is a program of sum of first (N) digit number, and (N) is entered by the user in `finaltest.c`. \
+3. This is a program of command line argument find the length of string that enter by the user in `cmdline.c`. \
+4. This is a program of find the area of circle using function in `area_of_circle.c`. \
+5. This is a Program to check the number is palindrome or not in `palindrome.c`.
--- palindrome.c
+++ palindrome.c
@@ -1,14 +1,16 @@
-int main(){
-    int number, temp, rem, sum = 0;
+// Program to check the number is palindrome or not.
+
+int main()                                                  //main function
+    int number, temp, rem, sum = 0;                         //variable declaration & initialization
     printf("Enter the palindrome number = \n");
     scanf("%d", &number);
     temp = number;
-    while(number > 0){
+    while(number > 0){                                      //while loop
         rem = number % 10;
         sum = (sum * 10) + rem;
         number = number / 10;
     }
-    if(temp == sum)
+    if(temp == sum)                                         //if else statement
         printf("Number is palindrome");
     else
         printf("Number is not palindrome");
